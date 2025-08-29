3b13ce1d03f79996ab0737919fae2de0018ab8ff
dabote <dabote@bancolombia.com.co>
2025-08-28 23:53:19 -0500

    Routes creation for leads component by lazyload

--- src/app/modules/leads/leads.module.ts
+++ src/app/modules/leads/leads.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterModule } from '@angular/router';
+import { leadsRoutes } from './leads.routes';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule.forChild(leadsRoutes)],
 })
-export class LeadsModule { }
+export class LeadsModule {}
---
+++ src/app/modules/leads/leads.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { LeadsComponent } from './ui/pages/leads/leads.component';
+
+export const leadsRoutes: Routes = [
+  {
+    path: '',
+    component: LeadsComponent,
+  },
+];
