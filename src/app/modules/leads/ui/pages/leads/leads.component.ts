08b8a39f97dba46534e16323914c0b9a82308d00
dabote <dabote@bancolombia.com.co>
2025-08-28 23:43:29 -0500

    File and folder structure

---
+++ src/app/modules/leads/ui/pages/leads/leads.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leads',
+  imports: [],
+  templateUrl: './leads.component.html',
+  styleUrl: './leads.component.scss',
+})
+export class LeadsComponent {}
