b4d78dfb3a05dff860852b78b9261d162c74daa5
dabote <dabote@bancolombia.com.co>
2025-08-31 22:16:15 -0500

    Se captura code después de redirección

--- src/app/modules/auth/ui/pages/auth/auth.component.ts
+++ src/app/modules/auth/ui/pages/auth/auth.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { AuthUsecaseService } from '../../../domain/usecase/auth-usecase.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -7,8 +8,21 @@ import { AuthUsecaseService } from '../../../domain/usecase/auth-usecase.service
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   private _authUsecaseService = inject(AuthUsecaseService);
+  private _activatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.listenToCode();
+  }
+
+  listenToCode() {
+    this._activatedRoute.queryParams.subscribe((params) => {
+      const code = params['code'] || null;
+      console.log('Code recibido:', code);
+      // TODO: Después de obtener el código se debe llamar el servicio del login
+    });
+  }
 
   getCode() {
     this._authUsecaseService.getCode('8315944344732576');
